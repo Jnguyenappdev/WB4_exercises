438478f90defdd7d2833beb5c6971487bdb7eb0e
Julle Nguyen <jnguyen@appdev.yearup.org>
2023-05-08 14:23:00 -0400

    I love loops

---
+++ ObjectScripts/LoopScripts/for_loops.js
@@ -0,0 +1,7 @@
+// print out the phrase "I love loops" 7 times
+
+let phrase = "I love loops";
+
+for (let i = 1 ; i < 8; i++) {
+    console.log(phrase, i);
+}
---
+++ ObjectScripts/LoopScripts/while_loops.js
@@ -0,0 +1,10 @@
+//use a while loopto print out the phrase "i love loops" 7 times
+
+
+let phrase = "I love loops";
+let i = 1 ;
+
+while ( i < 8 ) {
+    console.log(phrase , i);
+    i++
+}
